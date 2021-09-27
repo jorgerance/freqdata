c3fb9cd60c073343df8f1c52b6aac6f20b08be26
root <root@freqlab.jorge-rance.gmail.com.beta.tailscale.net>
2021-09-27 23:24:05 +0000

    auto update tickers

--- update_git.sh
+++ update_git.sh
@@ -3,7 +3,7 @@
 download_ftx() {
 	source /mnt/freqdata/freqtrade/.env/bin/activate
 	cd /mnt/freqdata/freqtrade
-	/mnt/freqdata/pythonlib/ftx_usd_pairs.py > ./user_data/ftx_usd_pairs.json
+	python /mnt/freqdata/pythonlib/ftx_usd_pairs.py > ./user_data/ftx_usd_pairs.json
 	freqtrade download-data -c user_data/ftx_usd_pairs.json --exchange ftx --days 100 --timeframes {1m,5m,15m,1h,4h,1d}
 }
 
